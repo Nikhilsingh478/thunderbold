0997f3ba75b0692dcddc053e18dcf45b9b66f298
Nikhilsingh478 <neelsingh45940s@gmail.com>
2026-04-08 11:22:26 +0530

    es module fixes

--- api/_lib/mongodb.js
+++ api/_lib/mongodb.js
@@ -1,56 +1,25 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
-if (!process.env.MONGO_URI) {
-  throw new Error('Please define the MONGO_URI environment variable inside .env.local');
+let cached = global.mongo;
+
+if (!cached) {
+  cached = global.mongo = { client: null, db: null };
 }
 
 export async function getDb() {
-  console.log("MONGO: Getting database connection...");
-  
-  if (global.mongo) {
-    console.log("MONGO: Using existing connection");
-    return global.mongo.db;
+  if (cached.db) return cached.db;
+
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI missing");
   }
 
-  const mongoUri = process.env.MONGO_URI;
-  console.log("MONGO: Checking environment variables...");
-  console.log("MONGO: MONGO_URI exists:", !!mongoUri);
-  
-  if (!mongoUri) {
-    console.error("MONGO: MONGO_URI environment variable is not defined");
-    throw new Error('MONGO_URI environment variable is not defined');
-  }
+  const client = new MongoClient(process.env.MONGO_URI);
+  await client.connect();
 
-  console.log("MONGO: Creating new MongoDB client...");
-  const client = new MongoClient(mongoUri);
-  
-  try {
-    console.log("MONGO: Connecting to MongoDB...");
-    await client.connect();
-    const db = client.db('thunderbold');
-    console.log("MONGO: Connected to database 'thunderbolt'");
-    
-    global.mongo = {
-      client,
-      db
-    };
-    
-    console.log("MONGO: Connection cached in global");
-    return db;
-  } catch (error) {
-    console.error('MONGO: Failed to connect to MongoDB:', error);
-    console.error('MONGO: Error details:', {
-      message: error.message,
-      name: error.name,
-      code: error.code
-    });
-    throw error;
-  }
-}
+  const db = client.db("thunderbold");
 
-export async function closeConnection() {
-  if (global.mongo) {
-    await global.mongo.client.close();
-    global.mongo = undefined;
-  }
+  cached.client = client;
+  cached.db = db;
+
+  return db;
 }
--- api/cart/index.js
+++ api/cart/index.js
@@ -1,6 +1,6 @@
-const { getDb } = require('../_lib/mongodb');
+import { getDb } from "../_lib/mongodb.js";
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -100,4 +100,4 @@ module.exports = async (req, res) => {
     });
     return res.status(500).json({ error: 'Internal server error' });
   }
-};
+}
